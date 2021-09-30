a7f447f16ce62ffb3dcf3af43235e9a3b0571723
Robbie08 <rortiz8838@sdsu.edu>
2021-09-29 17:06:54 -0700

    New: created new directory that includes client code HTML and JS, also cleaned up main.go

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.Info("Starting server...")
 	http.HandleFunc("/", defaultPage)
-	http.HandleFunc("/on", unlockSafe)
 	http.HandleFunc("/shutdown", shutdown)
 	http.ListenAndServe(":8080", nil)
 }
@@ -20,13 +19,28 @@ func main() {
 // Default handle for when server is spun up. You can think of this
 // as the home page
 func defaultPage(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		log.Info("Page not found")
-		return
+	switch r.Method {
+	case "GET":
+		path := r.URL.Path
+		fmt.Println(path)
+
+		if path == "/" {
+			path = "./client/index.html"
+		} else {
+			path = "." + path
+		}
+		http.ServeFile(w, r, path)
+
+	case "POST":
+		r.ParseMultipartForm(0)
+		message := r.FormValue("message")
+		fmt.Println("--------------------------------------------")
+		fmt.Println("Msg from client: ", message)
+		// we can make the calll to our piUtils.UnlockSafe() here!
+		piUtils.UnlockSafe()
+	default:
+		fmt.Println("This service only supports GET and POST requests")
 	}
-	log.Info("Someone hit the Default Page...")
-	http.ServeFile(w, r, "piInterface.html")
 }
 
 // This function is in charge of gracefully shutting down
@@ -34,12 +48,4 @@ func defaultPage(w http.ResponseWriter, r *http.Request) {
 func shutdown(w http.ResponseWriter, r *http.Request) {
 	log.Info("Shutting server down...")
 	os.Exit(0)
-}
-
-// This function is in charge of handling request from client
-// to open Smart Safe
-func unlockSafe(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "piInterface.html")
-	fmt.Println("Someone hit the unlock safe feature")
-	piUtils.UnlockSafe()
 }
