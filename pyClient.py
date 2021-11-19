618844ba1dfba2d5c5ad395c7423e3b569ff3699
Robbie08 <rortiz8838@sdsu.edu>
2021-11-19 15:45:07 -0800

    New: added python client file that sends passwords to user and go server

---
+++ pyClient.py
@@ -0,0 +1,16 @@
+import requests as req
+import random
+
+# This function will send our payload with password
+# @params: A password that we will send over to our http server
+def sendPayload(passwd):
+    payload = {'message': passwd }
+    req.post('http://localhost:8080/pyclient', params=payload)
+
+# This function will generate a password
+# @params length: determines the number of characters in our password
+# @return : generated password
+def generatePassword(length):
+    return ''.join([chr(random.randint(65,127)) for _ in range(length)])
+
+sendPayload(generatePassword(10)) # Generate password, package it into a payload and send it via http
